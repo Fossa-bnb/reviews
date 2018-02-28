19d6b926420d5cc98188ce98afb5c5914476cf73
Annisa <annisakaraca@gmail.com>
2018-02-28 12:02:21 -0800

    Calculate number of stars to display rounded to nearest .5

--- client/src/components/OverallStars.jsx
+++ client/src/components/OverallStars.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const OverallStars = props => (
-  <div>
-    Overall stars component for room {props.roomId}
-  </div>
-);
+const OverallStars = (props) => {
+  const roundedStars = Math.round(props.stars * 2) / 2;
+
+  return (
+    <div>
+    Number of stars: {roundedStars}
+    </div>
+  );
+};
 
 OverallStars.propTypes = {
-  roomId: PropTypes.number.isRequired,
+  // roomId: PropTypes.number.isRequired,
+  stars: PropTypes.number.isRequired,
 };
 
 export default OverallStars;

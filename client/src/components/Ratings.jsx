27955f3ff642c012fde564eabcd7939ac481d586
Annisa <annisakaraca@gmail.com>
2018-02-28 14:01:10 -0800

    Pass each subrating down through props and include PropTypes on Ratings component

--- client/src/components/Ratings.jsx
+++ client/src/components/Ratings.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Subrating from './Subrating';
 
-const Ratings = ({
-  accuracy, checkIn, cleanliness, communication, location, value,
-}) => (
+const Ratings = props => (
   <div>
-    Ratings Component
-    <Subrating category="Accuracy" rating={accuracy} />
-    <Subrating category="Check In" rating={checkIn} />
-    <Subrating category="Cleanliness" rating={cleanliness} />
-    <Subrating category="Communication" rating={communication} />
-    <Subrating category="Location" rating={location} />
-    <Subrating category="Value" rating={value} />
+    <Subrating category="Accuracy" rating={props.ratings.accuracy} />
+    <Subrating category="Check In" rating={props.ratings.checkIn} />
+    <Subrating category="Cleanliness" rating={props.ratings.cleanliness} />
+    <Subrating category="Communication" rating={props.ratings.communication} />
+    <Subrating category="Location" rating={props.ratings.location} />
+    <Subrating category="Value" rating={props.ratings.value} />
 
   </div>
 );

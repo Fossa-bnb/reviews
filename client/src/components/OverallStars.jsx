2dde38ef5c2462962f9f6f617c9ebb3541607a27
Annisa <annisakaraca@gmail.com>
2018-02-28 12:37:10 -0800

    Install react-stars package and use it with proper formatting to display overall rating

--- client/src/components/OverallStars.jsx
+++ client/src/components/OverallStars.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactStars from 'react-stars';
 
 const OverallStars = (props) => {
   const roundedStars = Math.round(props.stars * 2) / 2;
@@ -7,6 +8,15 @@ const OverallStars = (props) => {
   return (
     <div>
     Number of stars: {roundedStars}
+      <ReactStars
+        count={5}
+        edit={false}
+        half
+        value={roundedStars}
+        size={24}
+        color1="#D8D8D8"
+        color2="#008489"
+      />
     </div>
   );
 };

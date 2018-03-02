5bfbe87007e19daeb92c218a086188f4c51b638d
Annisa <annisakaraca@gmail.com>
2018-03-01 16:50:51 -0800

    Create Navigation subcomponent for viewing reviews on multiple pages

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Navigation = () => (
-  <div>
-    Navigation Component
-  </div>
-);
+const Navigation = (props) => {
+  const buttons = [];
+  for (let x = 0; x < props.pages; x += 1) {
+    const newBtn = (
+      <li className="page-item" key={x}>
+        <a className="page-link" href="#review" onClick={() => { props.clickHandler(x + 1); }} >
+          {x + 1}
+        </a>
+      </li>);
+    buttons.push(newBtn);
+  }
+  return (
+    <nav aria-label="reviews navigation">
+      <ul className="pagination">
+        {buttons}
+      </ul>
+    </nav>
+  );
+};
+
+Navigation.propTypes = {
+  pages: PropTypes.number.isRequired,
+  clickHandler: PropTypes.func.isRequired,
+};
 
 export default Navigation;

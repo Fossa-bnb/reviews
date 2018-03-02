8db8b177a3711aa5dc4ad1782e0e1525fa2a8a6e
Annisa <annisakaraca@gmail.com>
2018-03-01 16:51:18 -0800

    Make ReviewsList a stateful component to implement pagination

--- client/src/components/ReviewsList.jsx
+++ client/src/components/ReviewsList.jsx
@@ -4,14 +4,43 @@ import PropTypes from 'prop-types';
 import Review from './Review';
 import Navigation from './Navigation';
 
-const ReviewsList = props => (
-  <div>
-    {props.reviews.map(review => (
-      <Review review={review} key={review.reviewId} />
+class ReviewsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0,
+      reviews: this.props.reviews,
+      displayedReviews: this.props.reviews.slice(0, 7),
+    };
+    this.updateCurrentPage = this.updateCurrentPage.bind(this);
+  }
+
+  updateCurrentPage(newPage) {
+    this.setState({ page: newPage }, () => {
+      this.updateDisplayedReviews();
+    });
+  }
+
+  updateDisplayedReviews() {
+    const startIndex = (this.state.page - 1) * 7;
+    const endIndex = startIndex + 7;
+    this.setState({ displayedReviews: this.state.reviews.slice(startIndex, endIndex) });
+  }
+
+  render() {
+    return (
+      <div id="review">
+        {this.state.displayedReviews.map(review => (
+          <Review review={review} key={review.reviewId} />
       ))}
-    <Navigation />
-  </div>
-);
+        <Navigation
+          pages={Math.ceil(this.state.reviews.length / 7)}
+          clickHandler={this.updateCurrentPage}
+        />
+      </div>
+    );
+  }
+}
 
 ReviewsList.propTypes = {
   reviews: PropTypes.arrayOf(PropTypes.shape({

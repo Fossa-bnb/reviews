fb39209d82eef2540a04a33fd8349450af7b9743
Annisa <annisakaraca@gmail.com>
2018-03-05 12:21:11 -0800

    Render the gramatically correct message based on the number of search results

--- client/src/components/SearchSummary.jsx
+++ client/src/components/SearchSummary.jsx
@@ -1,25 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SearchSummary = ({ numberOfResults, params, resetSearch }) => (
-  <div className="container searchSummary">
-    <div className="row justify-content-between summaryText">
-      <div className="col-s-2">
-        {numberOfResults} guests have mentioned <span className="params">&quot;{params}&quot;</span>
+const SearchSummary = ({ numberOfResults, params, resetSearch }) => {
+  if (numberOfResults === 0) {
+    return (
+      <div className="container searchSummary">
+        <div className="row justify-content-between summaryText">
+          <div className="col-s-2">
+            None of our guests have mentioned <span className="params">&quot;{params}&quot;</span>
+          </div>
+          <div className="col-s-2">
+            <span
+              className="backToReviews"
+              onClick={() => resetSearch()}
+              onKeyUp={() => resetSearch()}
+              role="button"
+              tabIndex="0"
+            >Back to all reviews
+            </span>
+          </div>
+        </div>
       </div>
-      <div className="col-s-2">
-        <span
-          className="backToReviews"
-          onClick={() => resetSearch()}
-          onKeyUp={() => resetSearch()}
-          role="button"
-          tabIndex="0"
-        >Back to all reviews
-        </span>
+    );
+  } else if (numberOfResults === 1) {
+    return (
+      <div className="container searchSummary">
+        <div className="row justify-content-between summaryText">
+          <div className="col-s-2">
+            {numberOfResults} guest has mentioned <span className="params">&quot;{params}&quot;</span>
+          </div>
+          <div className="col-s-2">
+            <span
+              className="backToReviews"
+              onClick={() => resetSearch()}
+              onKeyUp={() => resetSearch()}
+              role="button"
+              tabIndex="0"
+            >Back to all reviews
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="container searchSummary">
+      <div className="row justify-content-between summaryText">
+        <div className="col-s-2">
+          {numberOfResults} guests have mentioned <span className="params">&quot;{params}&quot;</span>
+        </div>
+        <div className="col-s-2">
+          <span
+            className="backToReviews"
+            onClick={() => resetSearch()}
+            onKeyUp={() => resetSearch()}
+            role="button"
+            tabIndex="0"
+          >Back to all reviews
+          </span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 SearchSummary.propTypes = {
   numberOfResults: PropTypes.number.isRequired,

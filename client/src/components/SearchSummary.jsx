f5d40eb38e5ddd97f43000723512311a1d383ffd
Annisa <annisakaraca@gmail.com>
2018-03-04 17:52:56 -0800

    Add styling to searchSummary component

--- client/src/components/SearchSummary.jsx
+++ client/src/components/SearchSummary.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SearchSummary = ({ numberOfResults, params, resetSearch }) => (
-  <div className="searchSummary">
-    {numberOfResults} guests have mentioned <span className="params">{params}</span>
-    <span
-      className="backToReviews"
-      onClick={() => resetSearch()}
-      onKeyUp={() => resetSearch()}
-      role="button"
-      tabIndex="0"
-    >Back to all reviews
-    </span>
+  <div className="container searchSummary">
+    <div className="row justify-content-between">
+      <div className="col-s-2">
+        {numberOfResults} guests have mentioned <span className="params">{params}</span>
+      </div>
+      <div className="col-s-2">
+        <span
+          className="backToReviews"
+          onClick={() => resetSearch()}
+          onKeyUp={() => resetSearch()}
+          role="button"
+          tabIndex="0"
+        >Back to all reviews
+        </span>
+      </div>
+    </div>
   </div>
 );
 

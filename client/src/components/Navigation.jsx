c7afcd83abbffbff894f162550bc69513961e7c3
Annisa <annisakaraca@gmail.com>
2018-03-07 16:14:44 -0800

    Render page navigation bar using Bootstrap styling

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // import { pagination } from 'bootstrap-css';
+import Bootstrap from '../../styles/bootstrap.css';
 
 const Navigation = (props) => {
   const buttons = [];
   for (let x = 0; x < props.pages; x += 1) {
     const newBtn = (
-      <li className="page-item" key={x}>
-        <a className="page-link" href="#review" onClick={() => { props.clickHandler(x + 1); }} >
+      <li className={Bootstrap['page-item']} key={x}>
+        <a className={Bootstrap['page-link']} href="#review" onClick={() => { props.clickHandler(x + 1); }} >
           {x + 1}
         </a>
       </li>);
@@ -15,7 +16,7 @@ const Navigation = (props) => {
   }
   return (
     <nav aria-label="reviews navigation">
-      <ul className="pagination" >
+      <ul className={Bootstrap.pagination} >
         {buttons}
       </ul>
     </nav>
